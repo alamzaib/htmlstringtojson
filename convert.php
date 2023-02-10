1d486b0e61951c71acb058a66da9f1cc60289fd9
Alam <alam.zaib@nexsysone.com>
2023-02-10 11:59:55 +0400

    basic files

---
+++ convert.php
@@ -0,0 +1,7 @@
+<?php 
+echo 'HTML STRING TO JSON FORMAT';
+// todos
+// 1. fetch sample data
+// 2. call convertor function to process html code
+// 3. generate json string
+// 4. output in file/online
